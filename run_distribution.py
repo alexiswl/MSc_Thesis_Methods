231deb1dfe555134c3e2de0df173318ecbc05535
Alexis Lucattini <awlucattini@icloud.com>
2016-11-07 14:34:08 +1100

    Up to R stage on the run_distribution script. Just need to test how the file is printed first before continuing.

--- run_distribution.py
+++ run_distribution.py
@@ -8,7 +8,11 @@ from Bio import SeqIO
 import os
 
 main_directory = "/data/Bioinfo/bioinfo-proj-alexis/2016_08_16_E_COLI_R9/"
+analytics_directory = main_directory + "analytics/"
+if not os.path.isdir(analytics_directory):
+    os.mkdir(analytics_directory)
 
+DATE_PREFIX = str(time.strftime("%Y-%m-%d"))
 
 # Now, for each 2d read generated, where did that read go??
 pass_directory = main_directory + "reads/downloads/fail/"
@@ -38,4 +42,16 @@ fast5_files = [pass_directory + fast5_file for fast5_file in os.listdir(pass_dir
                 if fast5_file.endswith(".fast5")]
 distribution["pass"] += len(fast5_files)
 
-print(distribution)
+col_names = []
+col_values = []
+for keys, values in distribution.iterkeys():
+    col_names.append[keys]
+    col_values.append[values]
+
+run_distribution_summary_file = analytics_directory + DATE_PREFIX + "_" + RUN_NAME + "_run_distribution.txt"
+
+output_handle = open(run_distribution_summary_file, "w+")
+output_handle.write("\t".join(col_names) + "\n")
+output_handle.write("\t".join(col_values) + "\n")
+output_handle.close()
+
