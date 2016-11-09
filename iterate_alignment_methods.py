f2918b814706744596da72405a5b3c21cb8bf310
Alexis Lucattini <awlucattini@icloud.com>
2016-11-09 11:46:15 +1100

    iterate through all the possible alignment methods initial commit.

--- iterate_alignment_methods.py
+++ iterate_alignment_methods.py
@@ -6,10 +6,12 @@ basecalling_types = ("--1D --fasta", "--2D --fasta", "--fwd --fasta", "--rev --f
                      "--2D --fastq", "--2D --fastq --fail", "--fwd --fastq", "--fwd --fastq --fail"
                      "--rev --fastq", "--rev --fastq --fail")
 
-preliminary_commandline_arguments = "alignment.py --run_name E_COLI_R9 --working_directory ./ --reference ../references/.." \
+preliminary_commandline_arguments = "alignment.py --run_name E_COLI_R9 --working_directory ./ " + \
+                                    "--reference ../references/e_coli_k12_mg1655/NC_000913.fna " + \
                                     "--threads 20"
 
 for alignment_method in alignment_methods:
     for basecalling_type in basecalling_types:
-        full_commandline_argument = preliminary_commandline_arguments + " " + alignment_method + " " + basecalling_type
+        full_commandline_argument = preliminary_commandline_arguments + " --alignment_method " + alignment_method \
+                                    + " " + basecalling_type
         os.system(full_commandline_argument)
