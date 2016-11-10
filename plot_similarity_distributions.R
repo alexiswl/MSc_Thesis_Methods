b7ea951e9afaf9c8f0c39041d1a5426a45291907
Alexis Lucattini <awlucattini@icloud.com>
2016-11-10 21:33:36 +1100

    Adjusted plot similarity distributions so compatible on unix server.

--- plot_similarity_distributions.R
+++ plot_similarity_distributions.R
@@ -2,13 +2,13 @@
 
 main_directory = "/data/Bioinfo/bioinfo-proj-alexis/2016_08_2016_08_16_E_COLI_R9/waterman/"
 # Intracomparison test
-fwd_rev_fasta_failed <- read.table(main_directory + "intracomparison/fwd_rev/failed_quality_waterman_stats")
-twod_rev_fasta_failed <- read.table(main_directory + "intracomparison/2d_rev/failed_quality_waterman_stats")
-twod_fwd_fasta_failed <- read.table(main_directory + "intracomparison/2d_fwd/failed_quality_waterman_stats")
+fwd_rev_fasta_failed <- read.table(paste(main_directory, "intracomparison/fwd_rev/failed_quality_waterman_stats"))
+twod_rev_fasta_failed <- read.table(paste(main_directory, "intracomparison/2d_rev/failed_quality_waterman_stats"))
+twod_fwd_fasta_failed <- read.table(paste(main_directory, "intracomparison/2d_fwd/failed_quality_waterman_stats"))
 
-fwd_rev_fasta_pass <- read.table(main_directory + "intracomparison/fwd_rev/pass_waterman_stats")
-twod_rev_fasta_pass <- read.table(main_directory + "intracomparison/2d_rev/pass_waterman_stats")
-twod_fwd_fasta_pass <- read.table(main_directory + "intracomparison/2d_fwd/pass_waterman_stats")
+fwd_rev_fasta_pass <- read.table(paste(main_directory, "intracomparison/fwd_rev/pass_waterman_stats"))
+twod_rev_fasta_pass <- read.table(paste(main_directory, "intracomparison/2d_rev/pass_waterman_stats"))
+twod_fwd_fasta_pass <- read.table(paste(main_directory, "intracomparison/2d_fwd/pass_waterman_stats"))
 
 names(fwd_rev_fasta_failed) <- c("Filename", "Alignment_Score", "Similarity", "Identity")
 names(twod_rev_fasta_failed) <-  c("Filename", "Alignment_Score", "Similarity", "Identity")
@@ -34,7 +34,7 @@ legend("topleft",legend=legend_labels, col = rep(c("red","green","blue"),2), lty
 dev.off()
 
 # Cross comparison test 2d
-cross_2d = read.table(main_directory + "cross_comparison/2D_waterman_stats")
+cross_2d = read.table(paste(main_directory, "cross_comparison/2D_waterman_stats"))
 names(cross_2d) <- c("Read_name", "Alignment_Score", "Identity", "Similarity")
 
 png("2D_comparison_local_vs_cloud", type="cairo")
@@ -42,7 +42,7 @@ hist(as.numeric(sub("%", "", cross_2d$Identity)), main = "Histogram of Similarit
 dev.off()
 
 # Cross comparison test 1d
-cross_1d = read.table(main_directory + "cross_comparison/1D_waterman_stats")
+cross_1d = read.table(paste(main_directory, "cross_comparison/1D_waterman_stats"))
 names(cross_1d) <- c("Read_name", "Alignment_Score", "Identity", "Similarity")
 
 png("1D_comparison_local_vs_cloud", type="cairo")
