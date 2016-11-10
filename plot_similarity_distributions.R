0ea97516ee1c26a356efabef1193c78e98d3426e
Alexis Lucattini <awlucattini@icloud.com>
2016-11-10 21:38:54 +1100

    Adjusted plot similarity distributions so compatible on unix server.

--- plot_similarity_distributions.R
+++ plot_similarity_distributions.R
@@ -23,9 +23,9 @@ plot(density(as.numeric(sub("%", "", fwd_rev_fasta_pass$Similarity)), from = 0, 
 main = "Distribution of Similarity Scores", xlab = "Similarity Percentage")
 lines(density(as.numeric(sub("%", "", twod_rev_fasta_pass$Similarity))), col = "green")
 lines(density(as.numeric(sub("%", "", twod_fwd_fasta_pass$Similarity))), col = "red")
-lines(density(as.numeric(sub("%", "", fwd_rev_fasta_failed$Similarity))), col = "blue", lty = "2")
-lines(density(as.numeric(sub("%", "", twod_rev_fasta_failed$Similarity))), col = "green", lty = "2")
-lines(density(as.numeric(sub("%", "", twod_fwd_fasta_failed$Similarity))), col = "red", lty = "2")
+lines(density(as.numeric(sub("%", "", fwd_rev_fasta_failed$Similarity))), col = "blue", lty = 2)
+lines(density(as.numeric(sub("%", "", twod_rev_fasta_failed$Similarity))), col = "green", lty = 2)
+lines(density(as.numeric(sub("%", "", twod_fwd_fasta_failed$Similarity))), col = "red", lty = 2)
 
 legend_labels = c("2d vs fwd (pass)", "2d vs rev (pass)", "fwd vs rev (pass)",
                   "2d vs fwd (failed)", "2d vs rev (failed)", "fwd vs rev (rev)")
@@ -34,7 +34,7 @@ legend("topleft",legend=legend_labels, col = rep(c("red","green","blue"),2), lty
 dev.off()
 
 # Cross comparison test 2d
-cross_2d = read.table(paste(main_directory, "cross_comparison/2D_waterman_stats"))
+cross_2d = read.table(paste(main_directory, "cross_comparison/2D_waterman_stats", sep = ""))
 names(cross_2d) <- c("Read_name", "Alignment_Score", "Identity", "Similarity")
 
 png("2D_comparison_local_vs_cloud", type="cairo")
@@ -42,7 +42,7 @@ hist(as.numeric(sub("%", "", cross_2d$Identity)), main = "Histogram of Similarit
 dev.off()
 
 # Cross comparison test 1d
-cross_1d = read.table(paste(main_directory, "cross_comparison/1D_waterman_stats"))
+cross_1d = read.table(paste(main_directory, "cross_comparison/1D_waterman_stats", sep = ""))
 names(cross_1d) <- c("Read_name", "Alignment_Score", "Identity", "Similarity")
 
 png("1D_comparison_local_vs_cloud", type="cairo")
