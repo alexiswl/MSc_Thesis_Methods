4a31f29ffbcc578058afea24a06b200964fd1c48
Alexis Lucattini <awlucattini@icloud.com>
2016-11-12 12:07:18 +1100

    adjusted iterate alignment methods for compatability between different run chemistries

--- iterate_alignment_methods.py
+++ iterate_alignment_methods.py
@@ -2,11 +2,14 @@
 import os
 
 
-WORKING_DIRECTORY = "/data/Bioinfo/bioinfo-proj-alexis/"
+WORKING_DIRECTORY = "/data/Bioinfo/bioinfo-proj-alexis/2016_03_15_E_COLI_R7p3/"
 alignment_methods = ("bwa-mem", "graphmap", "last")
 
-basecalling_types = ("--1D --fasta", "--2D --fasta", "--fwd --fasta", "--rev --fasta",
-                     "--2D --fastq", "--2D --fastq --fail", "--fwd --fastq", "--fwd --fastq --fail",
+nanonet = False
+
+nanonet_basecalling_types = ("--2D --fasta", "--fwd --fasta", "--rev --fasta")
+
+metrichor_basecalling_types = ("--2D --fastq", "--2D --fastq --fail", "--fwd --fastq", "--fwd --fastq --fail",
                      "--rev --fastq", "--rev --fastq --fail")
 
 preliminary_commandline_arguments = "alignment.py --run_name E_COLI_R9 --working_directory ./ " + \
@@ -14,10 +17,13 @@ preliminary_commandline_arguments = "alignment.py --run_name E_COLI_R9 --working
                                     "--threads 20"
 
 for alignment_method in alignment_methods:
-    for basecalling_type in basecalling_types:
+    for basecalling_type in metrichor_basecalling_types:
         full_commandline_argument = preliminary_commandline_arguments + " --alignment_method " + alignment_method \
                                     + " " + basecalling_type
         os.system(full_commandline_argument)
 
-
-alignment_directory = WORKING_DIRECTORY + "alignment/"
+if nanonet:
+    for basecalling_type in nanonet_basecalling_types:
+        full_commandline_argument = preliminary_commandline_arguments + " --alignment_method " + alignment_method \
+                                    + " " + basecalling_type
+        os.system(full_commandline_argument)
